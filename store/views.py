d96391e3c2cc42f1b30c94b4f942c3fbae445279
mohamed nabil <mohamednabilabdo1@gmail.com>
2021-01-08 08:25:11 +0200

    Cart commit

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import * 
+import json
+
 
 def store(request):
 	products = Product.objects.all()
@@ -32,3 +35,27 @@ def checkout(request):
 
 	context = {'items':items, 'order':order}
 	return render(request, 'store/checkout.html', context)
+
+
+
+
+def update_item(request):
+	data = json.loads(request.body)
+	productId=data['productId']
+	action= data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+	orderItem.save()
+	
+
+	return JsonResponse("working",safe=False)
